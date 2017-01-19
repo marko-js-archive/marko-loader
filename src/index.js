0ccc4562871e8ef326582886c7b0b37860ca4adb
Michael Rawlings <ml.rawlings@gmail.com>
2017-01-18 22:22:21 -0800

    enable bundling for the server

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 var markoCompiler = require('marko/compiler');
+var loaderUtils = require('loader-utils');
 
 module.exports = function(source) {
+    var query = loaderUtils.parseQuery(this.query);
+
     this.cacheable(false);
 
-    if (markoCompiler.compileForBrowser) {
+    if (query.target !== 'server' && markoCompiler.compileForBrowser) {
         var compiled = markoCompiler.compileForBrowser(source, this.resourcePath, {
     		writeToDisk: false
     	});
         return compiled.code;
     } else {
         return markoCompiler.compile(source, this.resourcePath, {
-    		writeToDisk: false
+    		writeToDisk: false,
+            requireTemplates: true
     	});
     }
 
