6d900d32db237b920ca745c9fa68c1a2a00941de
Michael Rawlings <ml.rawlings@gmail.com>
2017-03-02 01:05:16 -0800

    add support for inline styles

---
+++ src/code-loader.js
@@ -0,0 +1,6 @@
+var loaderUtils = require('loader-utils');
+
+module.exports = function() {
+    var query = loaderUtils.parseQuery(this.query);
+    return query.code.replace(/\/n/g, '\n');
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 var markoCompiler = require('marko/compiler');
 var loaderUtils = require('loader-utils');
 
+var defaultLoaders = {
+    'css':'style-loader!css-loader!'
+}
+
 module.exports = function(source) {
     var query = loaderUtils.parseQuery(this.query);
+    var options = this.options;
+    var module = options && options.module;
+    var loaders = module && module.loaders || [];
 
     this.cacheable(false);
 
@@ -10,12 +17,60 @@ module.exports = function(source) {
         var compiled = markoCompiler.compileForBrowser(source, this.resourcePath, {
     		writeToDisk: false
     	});
-        return compiled.code;
+
+        var dependencies = compiled.dependencies.map((dependency, i) => {
+            if (!dependency.code) {
+                // external file, just require it
+                return `require('${dependency.path}');`
+            } else {
+                // inline content, we'll create a
+                var virtualPath = dependency.virtualPath;
+                var loader = getLoaderMatch(virtualPath, loaders);
+                var codeLoader = require.resolve('./code-loader');
+                var codeQuery = JSON.stringify({ code:dependency.code });
+                var loaderString = loaderUtils.stringifyRequest(this, `!!${loader}${codeLoader}?${codeQuery}!${this.resourcePath}`);
+                return `require(${loaderString})`;
+            }
+        })
+
+        return dependencies.concat(compiled.code).join('\n');
     } else {
         return markoCompiler.compile(source, this.resourcePath, {
     		writeToDisk: false,
             requireTemplates: true
     	});
     }
-
 };
+
+function getLoaderMatch(path, loaders) {
+    var loaderString;
+    var ext;
+
+    loaders.some(loader => {
+        if(loader.test.test(path)) {
+            loaderString = getLoaderString(loader.loader);
+            return true;
+        }
+    });
+
+    if (!loaderString) {
+        ext = path.slice(path.lastIndexOf('.')+1);
+        loaderString = getLoaderString(defaultLoaders[ext]);
+    }
+
+    return loaderString;
+}
+
+function getLoaderString(loader) {
+    if (!loader) {
+        return '';
+    } else if (typeof loader === 'string') {
+        return loader.slice(-1) === '!' ? loader : loader + '!';
+    } else if (Array.isArray(loader)) {
+        return loader.map(getLoaderString).join('');
+    } else {
+        var options = loader.options;
+        var optionsString = options && (typeof options === 'string' ? options : JSON.stringify(options));
+        return loader.loader + (optionsString ? '?' + optionsString : '') + '!';
+    }
+}
